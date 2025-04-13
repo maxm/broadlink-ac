69c629ba1c684d5a0d4f1a2bab1af13c172d1af0
Max <max@pomelogames.com>
2025-04-13 20:49:11 +0000

    fix off mode and update warnings

--- climate.py
+++ climate.py
@@ -66,9 +66,15 @@ class BroadlinkACClimate(ClimateEntity):
     async def async_update(self) -> None:
         """Fetch the latest state from the AC."""
         status = self._ac.get_ac_status(force_update=True)
+        if status is bool:
+            _LOGGER.error("Failed to get AC status")
+            return
         self._attr_current_temperature = status.get("ambient_temp")
         self._attr_target_temperature = status.get("temp")
-        self._attr_hvac_mode = self._map_mode_to_hvac(status.get("mode"))
+        if status.get("power") == "OFF":
+            self._attr_hvac_mode = HVACMode.OFF
+        else:
+            self._attr_hvac_mode = self._map_mode_to_hvac(status.get("mode"))
         self._attr_fan_mode = status.get("fanspeed").lower()
 
     async def async_set_temperature(self, **kwargs: Any) -> None:
@@ -76,19 +82,19 @@ class BroadlinkACClimate(ClimateEntity):
         if (temperature := kwargs.get(ATTR_TEMPERATURE)) is not None:
             self._ac.set_temperature(temperature)
             self._attr_target_temperature = temperature
-            await self.async_update_ha_state()
+            self.async_write_ha_state()
 
     async def async_set_hvac_mode(self, hvac_mode: HVACMode) -> None:
         """Set the HVAC mode."""
         self._ac.set_homeassistant_mode(str(hvac_mode))
         self._attr_hvac_mode = hvac_mode
-        await self.async_update_ha_state()
+        self.async_write_ha_state()
 
     async def async_set_fan_mode(self, fan_mode: str) -> None:
         """Set the fan mode."""
         self._ac.set_fanspeed(fan_mode.upper())
         self._attr_fan_mode = fan_mode
-        await self.async_update_ha_state()
+        self.async_write_ha_state()
 
     def _map_mode_to_hvac(self, mode: str) -> HVACMode:
         """Map AC mode to Home Assistant HVAC mode."""
